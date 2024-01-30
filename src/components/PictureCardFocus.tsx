64cc77504ba07f9f9782a8565f554faab94ca10e
QuentinJoanon <quentin.joanon@gmail.com>
2024-01-30 09:50:18 +0100

    save de reprise

--- src/components/PictureCardFocus.tsx
+++ src/components/PictureCardFocus.tsx
@@ -2,6 +2,7 @@ import { IonButton, IonCard, IonIcon, IonImg, IonItem } from "@ionic/react";
 import "./PictureCardFocus.scss";
 import { deletePhoto, downloadPhoto } from "../firebaseConfig";
 import { cloudDownloadOutline, trashOutline } from "ionicons/icons";
+import { saveAs } from 'file-saver'
 
 interface PictureCardProps {
   picture: string;
@@ -28,7 +29,7 @@ const PictureCardFocus: React.FC<PictureCardProps> = ({
     return urlParts[urlParts.length - 1].split("?")[0];
   };
 
-  async function downloadPicture(pictureUrl: string) {
+/*   async function downloadPicture(pictureUrl: string) {
     const downloadURL = await downloadPhoto(pictureUrl);
     if (downloadURL) {
       // Créer un élément a pour forcer le téléchargement
@@ -39,6 +40,25 @@ const PictureCardFocus: React.FC<PictureCardProps> = ({
       document.body.appendChild(a); // Ajoutez l'élément a à la page
       a.click(); // Simuler un clic pour télécharger
       document.body.removeChild(a); // Nettoyez en supprimant l'élément a
+    }
+  }
+
+  const handleDownload = () => {
+    // Create a Blob from the data and trigger download
+    const blob = new Blob([data], { type: 'text/x-vcard;charset=utf-8' })
+    const today = new Date()
+    const date =
+      today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate()
+    const time =
+      today.getHours() + ':' + today.getMinutes() + ':' + today.getSeconds()
+    const dateTime = date + '-' + time
+    saveAs(blob, `${dateTime}_card.vcf`)
+  } */
+
+  async function downloadPicture(pictureUrl: string) {
+    const downloadURL = await downloadPhoto(pictureUrl);
+    if (downloadURL) {
+      saveAs(downloadURL, getFileName(downloadURL));
     }
   }
 
